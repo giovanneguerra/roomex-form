d767e235e425547c402f6787b6d7bb597a743447
Giovanne Guerra <giovanneguerra@hotmail.com>
2022-11-13 23:10:43 +0000

    feature: Set app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnterComponent } from './components/enter/enter.component';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
 
-const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'enter', component: EnterComponent },
+  { path: 'thankyou', component: ThankYouComponent },
+  { path: '**', component: EnterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
